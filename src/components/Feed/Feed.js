b83d6d9eb8c0457355364439139227a5064a5f80
alyssagao1120 <alyssa.gao@yahoo.ca>
2022-01-12 22:02:27 -0500

    finish feed component

---
+++ src/components/Feed/Feed.js
@@ -0,0 +1,37 @@
+import './Feed.css';
+import { useState, useEffect} from "react";
+import Card from '../Card/Card';
+import fetchDataFromDateRange from '../fetchDataFromDateRange';
+import Loader from '../Loader/Loader';
+
+function Feed() {
+    const [data, setData] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [endDate, setEndDate] = useState(new Date());
+
+    useEffect(() => {
+        const fetchData = async () => {
+            let startDate = new Date(endDate);;
+            startDate = new Date(startDate.setDate(endDate.getDate() - 14))
+            setLoading(true);
+            const data = await fetchDataFromDateRange(startDate, endDate);
+            setLoading(false);
+            setData(data.reverse());
+        };
+        fetchData();
+    }, [endDate]);
+
+
+    return (
+        <div className="Feed">
+            {loading 
+                ?   <Loader/> 
+                :   data.map((cardData) => (
+                        <Card key={cardData.title} cardData={cardData}/>
+                    ))
+            }
+        </div>
+    );
+}
+
+export default Feed;
