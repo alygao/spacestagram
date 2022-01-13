448c3594ac738ebd04d3e5cbd860e72af373d52e
alyssagao1120 <alyssa.gao@yahoo.ca>
2022-01-12 22:03:18 -0500

    setup function to fetch data from NASA API

---
+++ src/components/fetchDataFromDateRange.js
@@ -0,0 +1,27 @@
+export default function fetchDataFromDateRange(startDate, endDate) {
+    // Retrieve all data within a 1 week range
+    // Convert startDate to YYYY-MM-DD format
+  const yearStart = startDate.getFullYear();
+  const monthStart = String(startDate.getMonth() + 1).padStart(2, "0");
+  const dayStart = String(startDate.getDate()).padStart(2, "0");
+  const startDateFormatted = yearStart + "-" + monthStart + "-" + dayStart;
+
+  // Convert endDate to YYYY-MM-DD format
+  const yearEnd = endDate.getFullYear();
+  const monthEnd = String(endDate.getMonth() + 1).padStart(2, "0");
+  const dayEnd = String(endDate.getDate()).padStart(2, "0");
+  const endDateFormatted = yearEnd + "-" + monthEnd + "-" + dayEnd;
+
+  console.log(startDate, endDate);
+  console.log(startDateFormatted, endDateFormatted);
+
+  const apiData = fetch(
+    `https://api.nasa.gov/planetary/apod?api_key=O7AHXLq890QyH9ZpqjSkcsbtrfoq0FPkYbWuFTkM&start_date=${startDateFormatted}&end_date=${endDateFormatted}`
+  )
+  .then((response) => response.json())
+  .then((fetchData) => {
+      return fetchData;
+  });
+
+  return apiData;
+}
